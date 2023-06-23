f26df21e5d1dacd9300d4c4f2c199f1479ae984b
gvdr <giulio.dallariva@canterbury.ac.nz>
2023-06-23 22:30:46 +1200

    misspell catch

--- src/Fechados/Month_of_Year.jl
+++ src/Fechados/Month_of_Year.jl
@@ -2,6 +2,6 @@ struct Month_of_Year <: AtomicFechado
     core::Atomic
 end
 
-Day_of_Month(n) = Day_of_Month(Atomic(n, collect(1:1:12), month))
+Month_of_Year(n) = Month_of_Year(Atomic(n, collect(1:1:12), month))
 
 succ(a_date::Tt,T::Month_of_Year,k) where {Tt<:TimeType} = a_date + Month(k)
